003f29534c7b821cedf515b76016e60ddf170381
Ana <anasiqueira.serpentis@gmail.com>
2026-08-08 00:05:22 -0300

    feat: update Swagger documentation theme from purple to saturn

--- api/src/config/swagger.ts
+++ api/src/config/swagger.ts
@@ -25,7 +25,7 @@ export async function registerSwagger(app: FastifyInstance) {
         routePrefix: DOCUMENTATION_ROUTE,
 
         configuration: {
-            theme: 'purple',
+            theme: 'saturn',
         },
     });
 }
